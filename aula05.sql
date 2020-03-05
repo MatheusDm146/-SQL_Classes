a3f8ebbc7ffb6097c1812fd9c9b6c19784c90dc7
MatheusDm146 <55902824+MatheusDm146@users.noreply.github.com>
2020-03-05 10:13:28 -0300

    Sql
    First class - SQL DDL Statements

---
+++ aula05.sql
@@ -0,0 +1,34 @@
+-- DDL - CREATE -- 
+
+SHOW DATABASES;
+
+USE INFORMATION_SCHEMA;
+
+SHOW TABLES;
+
+SELECT * FROM CHARACTER_SETS;
+
+SELECT * FROM COLLATIONS;
+
+
+CREATE DATABASE IF NOT EXISTS faculdade
+CHARACTER SET latin1
+COLLATE 'latin1_swedish_ci';
+
+USE faculdade;
+
+CREATE TABLE tbAlunos (
+rgm INT,
+nome VARCHAR(50), 
+email VARCHAR(50),     
+dtnascto DATE
+);
+
+drop table tbAlunos;
+
+CREATE TABLE tbAlunos (
+rgm INT PRIMARY KEY,
+nome VARCHAR(50) NOT NULL, 
+email VARCHAR(50) NOT NULL,     
+dtnascto DATE
+);
